a9fc346982fe7290367153bde01c84e79725ea22
Conrad Etherington <conrad.etherington@gmail.com>
2022-12-26 02:54:21 -0700

    commit tree 27b4dc8f1608bb51a866ddb2d3261b48674d0aed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,22 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+
     articles = Article.all.includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+
+      render json: articles, each_serializer: ArticleListSerializer
+
   end
 
   def show
+    session[:page_views] ||= 0
+    session[:page_views] = session[:page_views].to_i + 1
     article = Article.find(params[:id])
-    render json: article
+    unless session[:page_views] >= 4
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: 401 
+    end
   end
 
   private
